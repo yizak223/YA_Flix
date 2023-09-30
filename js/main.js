3b0c9f1bdc9e1fce1e2e90b5e08117a52a8131f2
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-30 20:43:28 +0300

    add some style

--- js/main.js
+++ js/main.js
@@ -5,15 +5,17 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjNjNzViMWI2ZDUwMGNkMjgzZjU0MmU4ZTFlZDJkYSIsInN1YiI6IjVjMDNiNDQwMGUwYTI2NDg2YTA2ZjYwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pj6auQlw-VfPG6PenA6MbujH_SQk3Xr3LmD6H9WdH04'
     }
   };
-  const title=document.querySelector('#title')
+  const moviesList=document.querySelector('#moviesList')
 
 const fetchMovies=(numPage=1)=>{ fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${numPage}`, options)
     .then(response => response.json())
     .then(data => 
        { console.log(data)
         data.results.forEach((movie,i) => {
-            title.innerHTML+=`<h1>${i+1}.${movie.title} </h1> 
+          moviesList.innerHTML+=`<div class='movieCard'>
             <img src='http://image.tmdb.org/t/p/w500${movie.poster_path}'>
+            <p>${movie.title} </p>
+            </div>
                                      `
         });
                 }
@@ -21,4 +23,5 @@ const fetchMovies=(numPage=1)=>{ fetch(`https://api.themoviedb.org/3/movie/popul
     .catch(err => console.error(err));
 }
 fetchMovies(4)
+
     
