69d86e48f2b5a9f8a9c35ccb48bea0e37f044c96
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-10-01 19:00:04 +0300

    add like

--- js/main.js
+++ js/main.js
@@ -6,68 +6,90 @@ const OPTIONS = {
   }
 };
 const MOVIE_LIST = document.querySelector('#moviesList')
-
-const FETCH_MOVIES = (numPage=1 , time = `day`) => {
+const favourite_movies=[]
+const FETCH_MOVIES = (numPage = 1, time = `day`) => {
   fetch(`https://api.themoviedb.org/3/trending/movie/${time}?language=en-US&page=${numPage}`, OPTIONS)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    data.results.forEach((movie) => {
-      MOVIE_LIST.innerHTML += `<div class='movieCard'>
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      data.results.forEach((movie) => {
+        MOVIE_LIST.innerHTML += `<div class='movieCard'>
                                 <img src='http://image.tmdb.org/t/p/w500${movie.poster_path}'>
-                                <p>${movie.original_title} </p>
+                                <button class='userLiked'>
+                                  <i  class="fa fa-heart" aria-hidden="true"></i>
+                                </button>
                                 </div>`})
-  })
-  .catch(err => console.error(err));
+      const USER_LIKED = document.querySelectorAll('.userLiked')
+      USER_LIKED.forEach((icon,i) => {
+        icon.addEventListener('click',()=>{
+          icon.classList.toggle("love"); 
+          favourite_movies.push(data.results[i].title)
+          if (typeof favourite === 'defined') {
+            localStorage.setItem('favouriteee',JSON.stringify(favourite_movies))
+            
+          } 
+          localStorage.setItem('favourite',JSON.stringify(favourite_movies))
+        })
+        
+      });
+
+    })
+    .catch(err => console.error(err));
 }
+console.log(localStorage.getItem('favourite'));
+console.log(JSON.parse(localStorage.getItem('favourite')));
+console.log(JSON.parse(localStorage.getItem('favouriteee')));
+// localStorage.clear();
+
 FETCH_MOVIES()
-const PAGINTION_BTN=document.querySelectorAll('.paginationBtn')
+const PAGINTION_BTN = document.querySelectorAll('.paginationBtn')
 
 
-let numPageUser=1;
-let paginationUser='day';
+let numPageUser = 1;
+let paginationUser = 'day';
 
 
 PAGINTION_BTN.forEach(BTN => {
-  BTN.addEventListener('click',()=>{ 
-      MOVIE_LIST.innerHTML=``
-      numPageUser=BTN.textContent
-      FETCH_MOVIES(numPageUser,paginationUser )
-      PAGINTION_BTN.forEach((btn) => {
-        btn.classList.remove('clicked');
-      })
-      BTN.classList.add('clicked')
-})});
-
-const INPT_PAGE=document.querySelector('#inptPage')
-INPT_PAGE.addEventListener('change',()=>{
-  MOVIE_LIST.innerHTML=`` 
-  numPageUser=INPT_PAGE.value
-  FETCH_MOVIES(numPageUser,paginationUser )
-})
-
-const SET_DAY=document.querySelector('#setDay')
-const SET_WEEK=document.querySelector('#setWeek')
-
-
-
-SET_DAY.addEventListener('click',()=>{
-  MOVIE_LIST.innerHTML=`` 
-  paginationUser='day'
-    FETCH_MOVIES(numPageUser , 'day') 
+  BTN.addEventListener('click', () => {
+    MOVIE_LIST.innerHTML = ``
+    numPageUser = BTN.textContent
+    FETCH_MOVIES(numPageUser, paginationUser)
     PAGINTION_BTN.forEach((btn) => {
       btn.classList.remove('clicked');
     })
-    PAGINTION_BTN[numPageUser-1].classList.add('clicked')
+    BTN.classList.add('clicked')
+  })
+});
 
+const INPT_PAGE = document.querySelector('#inptPage')
+INPT_PAGE.addEventListener('change', () => {
+  MOVIE_LIST.innerHTML = ``
+  numPageUser = INPT_PAGE.value
+  FETCH_MOVIES(numPageUser, paginationUser)
 })
 
-SET_WEEK.addEventListener('click',()=>{
-  MOVIE_LIST.innerHTML=`` 
-  paginationUser='week'
-  FETCH_MOVIES(numPageUser , paginationUser) 
+const SET_DAY = document.querySelector('#setDay')
+const SET_WEEK = document.querySelector('#setWeek')
+
+
+
+SET_DAY.addEventListener('click', () => {
+  MOVIE_LIST.innerHTML = ``
+  paginationUser = 'day'
+  FETCH_MOVIES(numPageUser, 'day')
   PAGINTION_BTN.forEach((btn) => {
     btn.classList.remove('clicked');
   })
-  PAGINTION_BTN[numPageUser-1].classList.add('clicked')
+  PAGINTION_BTN[numPageUser - 1].classList.add('clicked')
+
+})
+
+SET_WEEK.addEventListener('click', () => {
+  MOVIE_LIST.innerHTML = ``
+  paginationUser = 'week'
+  FETCH_MOVIES(numPageUser, paginationUser)
+  PAGINTION_BTN.forEach((btn) => {
+    btn.classList.remove('clicked');
+  })
+  PAGINTION_BTN[numPageUser - 1].classList.add('clicked')
 })
