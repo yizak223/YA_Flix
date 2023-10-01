de580f57525e3d8993218c29dc2edc6a3ecf3380
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-10-01 03:06:20 +0300

    add trending

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjNjNzViMWI2ZDUwMGNkMjgzZjU0MmU4ZTFlZDJkYSIsInN1YiI6IjVjMDNiNDQwMGUwYTI2NDg2YTA2ZjYwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pj6auQlw-VfPG6PenA6MbujH_SQk3Xr3LmD6H9WdH04'
-    }
+      accept:' application/json',
+      Authorization:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDMwZDdkNmE3NWVmMjUzYjE3MWQxMzE0ZTNiOGY4ZiIsInN1YiI6IjY1MTViNTEwOTNiZDY5MDEzOGZjNjFjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DnXvPBDGXfiOwqal9-lzl2zqjV-mLbJGJhCTg3gX7Vo' 
+      }
   };
   const moviesList=document.querySelector('#moviesList')
 
-const fetchMovies=(numPage=1)=>{ fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${numPage}`, options)
+const fetchMovies=(numPage=1,time=`day`)=>{ fetch(`https://api.themoviedb.org/3/trending/movie/${time}?language=en-USpage=${numPage}`, options)
     .then(response => response.json())
     .then(data => 
        { console.log(data)
-        data.results.forEach((movie,i) => {
+        data.results.forEach((movie) => {
           moviesList.innerHTML+=`<div class='movieCard'>
-            <img src='http://image.tmdb.org/t/p/w500${movie.poster_path}'>
-            <p>${movie.title} </p>
-            </div>
-                                     `
-        });
-                }
-        )
+                                <img src='http://image.tmdb.org/t/p/w500${movie.poster_path}'>
+                                <p>${movie.title} </p>
+                                </div>`})})
     .catch(err => console.error(err));
 }
-fetchMovies(4)
+fetchMovies(4,'week')
 
     
