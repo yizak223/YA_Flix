152241e7910a4551fb5d5681c9222615d118617b
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-10-02 14:22:42 +0300

    start 2 page

--- js/singleMovie.js
+++ js/singleMovie.js
@@ -1,7 +1,16 @@
-const singleMovieFetch = (movieId = 12) => {
-    fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=bc1f09cc15ddc7d57d1d665f10e1e5a6&language=en-US&append_to_response=credits`)
-    .then(response => {response.json()})
-    .then(movieData => { console.log(movieData); })
-    .catch(err => console.error(err))
-}
-singleMovieFetch()
+const MOVIE_ID=document.querySelector('#movieById')
+const BACKG_IMG=document.querySelector('#backgImgContainer')
+
+
+const MOVIE_ID_FETCH=(movie_id=12)=>{
+    fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=bc1f09cc15ddc7d57d1d665f10e1e5a6&language=en-US&append_to_response=credits`)
+        .then(response => response.json())
+        .then(movieData => {
+            console.log(movieData)
+            BACKG_IMG.innerHTML+=`<img id='backgImg' src='http://image.tmdb.org/t/p/w500${movieData.backdrop_path}'>`
+            MOVIE_ID.innerHTML+=`<div id='containerMovie'> 
+            <img style='position: absolute;filter:0' src='http://image.tmdb.org/t/p/w500${movieData.poster_path}'>
+            </div>
+           `; 
+        } )}
+        MOVIE_ID_FETCH()
