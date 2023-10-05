c88dfb52ebec66cf9c7dba741954514ff16e9c98
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-10-05 19:02:19 +0300

    add feature

--- js/favorite.js
+++ js/favorite.js
@@ -6,38 +6,47 @@ const OPTIONS = {
     headers: {
         accept: ' application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDMwZDdkNmE3NWVmMjUzYjE3MWQxMzE0ZTNiOGY4ZiIsInN1YiI6IjY1MTViNTEwOTNiZDY5MDEzOGZjNjFjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DnXvPBDGXfiOwqal9-lzl2zqjV-mLbJGJhCTg3gX7Vo'
-    }}
-    const FETCH_MOVIE = (idMovie) => {
-        fetch(`https://api.themoviedb.org/3/movie/${idMovie}?language=en-US`, OPTIONS)
-            .then(response => response.json())
-            .then(movie => {
-                console.log(movie)
-                const isLiked = favourite_movies.includes(movie.id); // Check if the movie ID is liked
-                const likeButtonClass = isLiked ? 'userLiked' : '';
-                FAVORITE_CONTAINER.innerHTML += `<div class='movieCardList'>
+    }
+}
+const FETCH_MOVIE = (idMovie) => {
+    fetch(`https://api.themoviedb.org/3/movie/${idMovie}?language=en-US`, OPTIONS)
+        .then(response => response.json())
+        .then(movie => {
+            console.log(movie)
+            const isLiked = favourite_movies.includes(movie.id); 
+            const likeButtonClass = isLiked ? 'userLiked' : '';
+            FAVORITE_CONTAINER.innerHTML += `<div class='movieCardList'>
             <img src='http://image.tmdb.org/t/p/w500${movie.poster_path}'>
             <button class='likeBtn ${likeButtonClass}'> <i class="fa fa-thumbs-up" aria-hidden="true"></i> like</button>
             <span class='idMovieContainer'>ID: ${movie.id}</span>
           </div>`
-          const USER_LIKED = document.querySelectorAll('.likeBtn');
-          USER_LIKED.forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-              btn.classList.toggle('userLiked');
-              const movieId = movie.id;
-              const movieIndex = favourite_movies.indexOf(movieId);
-              if (movieIndex === -1) {
-                favourite_movies.push(movieId);
-              } else {
-                favourite_movies.splice(movieIndex, 1);
-              }
-              localStorage.setItem('favourite', JSON.stringify(favourite_movies));
+            const USER_LIKED = document.querySelectorAll('.likeBtn');
+            USER_LIKED.forEach((btn, i) => {
+                btn.addEventListener('click', () => {
+                    btn.classList.toggle('userLiked');
+                    const movieId = movie.id;
+                    const movieIndex = favourite_movies.indexOf(movieId);
+                    if (movieIndex === -1) {
+                        favourite_movies.push(movieId);
+                    } else {
+                        const userConfirmed = confirm('Are you sure you want to remove the movie from your favorites?');
+                        if (userConfirmed) {
+                            favourite_movies.splice(movieIndex, 1);
+                            
+                            location.reload();
+                        } else {
+                            btn.classList.add('userLiked');
+                            return;
+                        }
+                    }
+                    localStorage.setItem('favourite', JSON.stringify(favourite_movies));
+                });
             });
-          });
-            })
-            
-            .catch(err => console.error(err));
-    }
-    
+        })
+
+        .catch(err => console.error(err));
+}
+
 if (favourite_movies.length > 0) {
     console.log(true);
     FAVORITE_CONTAINER.innerHTML = ``
